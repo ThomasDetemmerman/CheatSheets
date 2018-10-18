3557518869fd2ffc455c2dac3d3292926ce86680
Thomas <detemmerman.thomas@hotmail.com>
2018-10-18 23:10:26 +0200

    Update readfile.pl

--- perl/readfile.pl
+++ perl/readfile.pl
@@ -1,24 +1,56 @@
 #!/usr/bin/perl
+$^I = ".bak";
 
-#override current file and create backup
-$^I=".bak";
+my $url     = 'http://www.sunrise-and-sunset.com/nl/sun/belgie/ninove';
+my $logfile = '/var/log/sunsetManager.log';
+@ARGV = "file";    #'/boot/config.txt'
 
-#override given parameters
-@ARGV="boot/config.txt";
+my $html = qx{wget --quiet --output-document=- $url};
+$html =~ s/\s//g;    #remove all whitespace
 
-#diamond opperator: given parameter is a file
-while(<>){
-    
-    #if line matches this regex
-    #one number is catched due to parentheses
-    if(/disable_camera_led=(\d)/){
-        
-        #Value of the parentheses is accessed by $1
-        $set = $1==1 ? 0 : 1;
-        print "disable_camera_led=$set\n";
-     
-    #we do nothing with all other lines of the file and simply place them back by printing themout
-    }else{
-       print $_;
+#array (hour, min)
+@zonsopgang    = $html =~ /<th>Zonsopgangvandaag<\/th><td>(\d\d):(\d\d)/;
+@zonsondergang = $html =~ /<th>Zonsondergangvandaag<\/th><td>(\d\d):(\d\d)/;
+
+#remove leading zero's
+foreach (@zonsopgang)    { $_ =~ s/^0//; }
+foreach (@zonsondergang) { $_ =~ s/^0//; }
+
+if ( timeMatch(@zonsondergang) || timeMatch(@zonsondergang) ) {
+    while (<>) {
+
+        #if line matches this regex
+        #one number is catched due to parentheses
+        if (/disable_camera_led=(\d)/) {
+
+            #Value of the parentheses is accessed by $1
+            $set = $1 == 1 ? 0 : 1;
+            print "disable_camera_led=$set\n";
+         }
+        else {
+            #we do nothing with all other lines of the file and simply place them back by printing themout
+            print $_;
+        }
+    }
+}
+
+#usage: timeMatch($hour,$minutes)
+sub timeMatch {
+    my ( $parahour, $paramin ) = @_;
+    my (
+        $sec,  $currentmin, $currenthour, $mday, $mon,
+        $year, $wday,       $yday,        $isdst
+    ) = localtime(time);
+    $decimalvalueofparamin    = substr( $paramin,    0, 1 );
+    $decimalvalueofCurrentmin = substr( $currentmin, 0, 1 );
+
+    # 3:33 matches 3:39, therefor, the program should run every 10 minutes.
+    if (   $parahour == $currenthour
+        && $decimalvalueofCurrentmin == $decimalvalueofparamin )
+    {
+        return true;
+    }
+    else {
+        return false;
     }
 }
