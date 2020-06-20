c79a21c1c8342d3dcec858a27cb6b63eee64958f
Thomas <detemmerman.thomas@hotmail.com>
2020-06-20 11:19:22 +0200

    Update sunsetmanager.sh

--- Raspberrypi/sunsetmanager.sh
+++ Raspberrypi/sunsetmanager.sh
@@ -1,5 +1,5 @@
 #When should this script be checked
-# - Not equal or less then 2.5 minutes, in such cases it's possible that the scrirpt runs two times whithin the same 5 minutes timeframe
+# - Not equal or less then 2.5 minutes, in such cases it's possible that the scrirpt runs two times within the same 5 minutes timeframe
 # - Not more than 5 minutes, since it can miss the five minute time frame.
 # recommended value: 4 (in crontab */4 * * * *)
 fiveMin=$((5*60))
@@ -20,12 +20,12 @@ if [ $(($sunrise - $now))  -lt $fiveMin ] && [ $(($sunrise - $now))  -gt 0 ]
 then 
     echo "$(date): sunrise whitin 5 minutes ($sunrisedate). Dissabling camera led" >> /data/usr/sunsetmanager.log
     sed -i -e 's/disable_camera_led=0/disable_camera_led=1/g' /boot/config.txt
-    shutdown -r +5 "toggeling infrared camera due to sunset. Rebooting withing 5 mins"
+    shutdown -r +5 "toggeling infrared camera due to sunset. Rebooting within 5 mins"
 fi
 
 if [ $(($sunset - $now))  -lt $fiveMin ] && [ $(($sunset - $now))  -gt 0 ]
 then
     echo "$(date): sunset whitin 5 minutes ($sunsetdate). Enabling camera led" >> /data/usr/sunsetmanager.log
     sed -i -e 's/disable_camera_led=1/disable_camera_led=0/g' /boot/config.txt
-    shutdown -r +5 "toggeling infrared camera due to sunset. Rebooting withing 5 mins"
+    shutdown -r +5 "toggeling infrared camera due to sunset. Rebooting within 5 mins"
 fi
